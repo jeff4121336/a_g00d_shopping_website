43a01993d1975640e62b95e424d119d2082ca1a1
jeff4121336 <jeffyau36@gmail.com>
2023-02-17 02:10:37 +0800

    17/02/23 ver7.1

--- admin.php
+++ admin.php
@@ -1,6 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    require DIR.'/lib/db.inc.php';
+    $res = ierg4210_cat_fetchall();
+    $options = '';
+    foreach ($res as $value){
+    $options .= '<option value="'.$value["cid"].'"> '.$value["name"].' </option>';
+    }
+?>
 
+<html lang="en">
 <head>
     <meta charset="UTF-8"> 
     <title>GoodShop</title>
@@ -11,15 +18,7 @@
 
 <body>
 
-<?php
-    require __DIR__.'/lib/db.inc.php';
-    $res = ierg4210_cat_fetchall();
-    $options = '';
-    foreach ($res as $value){
-    $options .= '<option value="'.$value["cid"].'"> '.$value["name"].' </option>';
-    }
-echo date('h:i:s A');
-?>
+
 
     <div> 
         <header> Welcome to GoodShop! </header>
@@ -36,19 +35,19 @@ echo date('h:i:s A');
         <legend> New Product</legend>
         <form id="prod_insert" method="POST" action="admin-process.php?action=prod_insert"
         enctype="multipart/form-data">
-            <label for="prod_cid"> Category *</label>
+            <label for="prod_cid"> Category </label>
             <div> <select id="prod_cid" name="cid"><?php echo $options; ?></select></div>
 
-            <label for="prod_name"> Name *</label>
-            <div> <input id="prod_name" type="text" name="name" required="required" pattern="^[\w\-]+$"/></div>
+            <label for="prod_name"> Name</label>
+            <div> <input id="prod_name" type="text" name="name" required="required" pattern="^[\w-]+$"/></div>
 
-            <label for="prod_price"> Price *</label>
-            <div> <input id="prod_price" type="text" name="price" required="required" pattern="^\d+\.?\d*$"/></div>
+            <label for="prod_price"> Price </label>
+            <div> <input id="prod_price" type="text" name="price" required="required" pattern="^\d+.?\d$"/></div>
 
-            <label for="prod_desc"> Description *</label>
+            <label for="prod_desc"> Description </label>
             <div> <input id="prod_desc" type="text" name="description"/> </div>
 
-            <label for="prod_image"> Image * </label>
+            <label for="prod_image"> Image </label>
             <div> <input type="file" name="file" required="true" accept="image/jpeg"/> </div>
 
             <input type="submit" value="Submit"/>
@@ -58,8 +57,33 @@ echo date('h:i:s A');
         
         <div id="FlexCol2">
 
+            <fieldset>
+                <legend>New Catagories</legend>
+                <form method="POST" action=“admin-process.php?action=xxx" enctype="multipart/form-data">
+
+                    <label for="prod_cat">Category *</label>
+                    <div>
+                        <input id="prod_cat" name="catname" type="text" required="true" pattern="[a-zA-Z\s]+"></select>
+                    </div>
+
+                    <input type="submit" value="Submit" />
+                </form> 
+            </fieldset>
+
+            <fieldset>
+                <legend>Delete Product</legend>
+                <form method="POST" action=“admin-process.php?action=xxx" enctype="multipart/form-data">
+                    
+                    <label for="prod_name">Product Name  *</label>
+                    <div>
+                        <select id="prod_name" name="name" required="true"></select>
+                    </div>
+
+                    <input type="submit" value="Submit" />
+                </form> 
+            </fieldset>
         </div>
-    
+
     </div>
 
     <div>
--- lib/db.inc.php
+++ lib/db.inc.php
@@ -3,7 +3,7 @@ function ierg4210_DB() {
 	// connect to the database
 	// TODO: change the following path if needed
 	// Warning: NEVER put your db in a publicly accessible location
-	$db = new PDO('sqlite:/var/www/cart.db');
+	$db = new PDO('sqlite:var/www/cart.db');
 
 	// enable foreign key support
 	$db->query('PRAGMA foreign_keys = ON;');
